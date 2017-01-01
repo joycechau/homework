ce7bb449f70320a02eb762f6e27b441fcc75e6a4
Joyce Chau <jyc.chau@gmail.com>
2016-12-31 19:17:40 -0800

    Complete w6d3 HW: First Ajax Request

--- w6d3/first_ajax_skeleton/js/index.js
+++ w6d3/first_ajax_skeleton/js/index.js
@@ -1,5 +1,15 @@
 console.log("Hello from the JavaScript console!");
 
-// Your AJAX request here
 
+// Your AJAX request here
+$.ajax({
+  type: 'GET',
+  url: "http://api.openweathermap.org/data/2.5/weather?q=NY,NY&appid=bcb83c4b54aee8418983c2aff3073b3b",
+  success(res) {
+    console.log("We have your weather!");
+    console.log(res);
+  }
+});
 // Add another console log here, outside your AJAX request
+
+console.log("I love corgis!");
